0be8caf18751c7a432303947b323eb50de1d25c2
albracu <produccion9@softpymes.com.co>
2016-04-27 23:07:48 -0500

    second commit

--- src/agenda/Agenda.java
+++ src/agenda/Agenda.java
@@ -26,13 +26,13 @@ public class Agenda extends JFrame implements ActionListener{
     
     //Texto de Introduccion
     nombreprograma = new JLabel("Agenda Telefonica");
-        nombreprograma.setBounds(0,0,180,30);
+        nombreprograma.setBounds(150,30,180,30);
         add(nombreprograma);
     nombreautor = new JLabel("Alfredo Bravo Cuero");
-        nombreautor.setBounds(0,30,180,30);
+        nombreautor.setBounds(145,60,180,30);
         add(nombreautor);
     numeroversion = new JLabel("version 1.0");
-        numeroversion.setBounds(0,50,180,30);
+        numeroversion.setBounds(170,90,180,30);
         add(numeroversion);
     
     //Menu Superior
@@ -56,13 +56,15 @@ public class Agenda extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent e){
         Container f=this.getContentPane();
         if(e.getSource()==mi1){
+            //Ocultar elementos Previos
             nombreprograma.setVisible(false);
             nombreautor.setVisible(false);
             numeroversion.setVisible(false);
             
-            //Formulario
-            etiquetanuevonombre = new JLabel("Nuevo Nombre");
-                etiquetanuevonombre.setBounds(0,0,180,30);
+            
+            //Formulario Crear
+            etiquetanuevonombre = new JLabel("Nuevo Nombre: ");
+                etiquetanuevonombre.setBounds(10,0,180,30);
                 add(etiquetanuevonombre);
                 etiquetanuevonombre.setVisible(true);
                 
@@ -72,31 +74,107 @@ public class Agenda extends JFrame implements ActionListener{
                 camponuevonombre.setVisible(true);
             
             etiquetanuevotelefono = new JLabel("Nuevo Telefono");
-                etiquetanuevotelefono.setBounds(0,50,180,30);
+                etiquetanuevotelefono.setBounds(10,50,180,30);
                 add(etiquetanuevotelefono);
                 etiquetanuevotelefono.setVisible(true);
             
-           camponuevonombre = new JTextField();
-                camponuevonombre.setBounds(200,50,180,30);
-                add(camponuevonombre);
-                camponuevonombre.setVisible(true);
+           camponuevotelefono = new JTextField();
+                camponuevotelefono.setBounds(200,50,180,30);
+                add(camponuevotelefono);
+                camponuevotelefono.setVisible(true);
                 
             botonnuevo = new JButton("Crear");
                 botonnuevo.setBounds(200,100,180,30);
                 add(botonnuevo);
+                botonnuevo.addActionListener(this);
                 botonnuevo.setVisible(true);
+                
+         
         }
         if(e.getSource()==mi2){
+            //Ocultar elementos Previos
+            etiquetanuevonombre.setVisible(false);
+            camponuevonombre.setVisible(false);
+            etiquetanuevotelefono.setVisible(false);
+            camponuevotelefono.setVisible(false);
+            botonnuevo.setVisible(false);
             
+               //Formulario Buscar
+            imprimenombre = new JLabel("Buscar por Nombre: ");
+                imprimenombre.setBounds(10,0,180,30);
+                add(imprimenombre);
+                imprimenombre.setVisible(true);
+                
+            campobuscarnombre = new JTextField();
+                campobuscarnombre.setBounds(200,0,180,30);
+                add(campobuscarnombre);
+                campobuscarnombre.setVisible(true);
+            
+            botonbusca = new JButton("Buscar");
+                botonbusca.setBounds(200,50,180,30);
+                add(botonbusca);
+                botonbusca.addActionListener(this);
+                botonbusca.setVisible(true);
         }
         if(e.getSource()==mi3){
-          
+            System.exit(0);
         }
  
         if(e.getSource()==botonnuevo){
-          
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/agendatelefonica", "root", "lol12345");
+                Statement estado = con.createStatement();
+                estado.executeUpdate("INSERT INTO agenda VALUES ('2','"+camponuevonombre.getText()+"','"+camponuevotelefono.getText()+"')");
+            }catch(SQLException ex){
+                System.out.println("Error de Mysql");
+            }catch(ClassNotFoundException err){
+                err.printStackTrace();
+            }catch(Exception err){
+                System.out.println("Se ha encontrado un error inesperado: "+err.getMessage());
+            }
+        }
+        
+        if(e.getSource()==botonbusca){
+            //Conectar a la Base de Datos
+             try{
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/agendatelefonica", "root", "lol12345");
+                Statement estado = con.createStatement();
+                ResultSet resultado = estado.executeQuery("SEÑECT * FROM agenda WHERE nombre = '"+campobuscarnombre.getText() +"')");
+                
+                //Exportar el Resultado
+                while(resultado.next()){
+                    if(archivo.exists()){
+                        if(archivo.canWrite()){
+                            nuevoarchivo = new Formatter("/home/yo/Escritorio/archivo.txt");
+                            nuevoarchivo.format("%s %s %s", resultado.getString("nombre"),resultado.getString("telefono"),"telefono");
+                            nuevoarchivo.close();
+                        }else{
+                            System.out.println("El Archivo existe pero no puedo usarlo");
+                        }                        
+                        }else{
+                            try{
+                                nuevoarchivo = new Formatter("/home/yo/Escritorio/archivo.txt");
+                                nuevoarchivo.format("%s %s %s", resultado.getString("nombre"),resultado.getString("telefono"),"telefono");
+                                nuevoarchivo.close();
+                            }catch(Exception errr){
+                                System.out.println("Error: "+ errr);
+                            }
+                        }
+                                   
+                    }
+                }catch(SQLException ex){
+                    System.out.println("Error de Mysql");
+                }catch(ClassNotFoundException err){
+                    err.printStackTrace();
+                }catch(Exception err){
+                    System.out.println("Se ha encontrado un error inesperado: "+err.getMessage());
+                }
+            
         }
     }
+    
     public static void main(String[] args) {
         Agenda ventana = new Agenda();
         ventana.setBounds(10,20,450,250);
